79532ecd4db42fb067babbc1efe855ba2bc4ab38
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-09-15 19:25:25 +0000

    Add todo list and add item functionality

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin  # noqa: F401
+from .models import Item
+
 
 # Register your models here.
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,10 @@
 from django.db import models  # noqa: F401
 
+
 # Create your models here.
-# class Item(models.Model):
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
